44647d6fe95478d77eea98fe632599fa8cf4fd38
ChicoTI <chico_emanuelsoares@hotmail.com>
2025-11-17 16:17:29 -0300

    erro

--- src/app/departamentos/departamento.service.ts
+++ src/app/departamentos/departamento.service.ts
@@ -8,23 +8,26 @@ export interface Departamento {
 }
 
 @Injectable({
-  providedIn: 'root' 
+  providedIn: 'root',
 })
 export class DepartamentoService {
-  private apiUrl = 'http://localhost:8080/departamentos'; 
+  private apiUrl = 'http://localhost:8080/departamentos';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getDepartamentos(): Observable<Departamento[]> {
     return this.http.get<Departamento[]>(this.apiUrl);
   }
 
   addDepartamento(departamento: Departamento): Observable<Departamento> {
-    return this.http.post<Departamento>(`${this.apiUrl}/criar`, departamento);
+    return this.http.post<Departamento>(`${this.apiUrl}`, departamento);
+  }
+
+  atualizarDepartamento(id: number, departamento: Departamento): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, departamento);
   }
 
   deleteDepartamento(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
-
 }
